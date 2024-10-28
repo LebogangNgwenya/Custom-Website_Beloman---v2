ac0ca70f4b7dd86f15326be7b86d6072c1243b92
Lebogang Ngwenya <109143680+LebogangNgwenya@users.noreply.github.com>
2024-10-28 17:06:38 +0200

    up2

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //call footer
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('../Custom-Website_Beloman---v2/components/footer.html')
+    fetch('/components/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer').innerHTML = data;
